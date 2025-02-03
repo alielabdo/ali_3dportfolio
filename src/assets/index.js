2165efe4bb3a43e566fa95953dcf681e304f233c
alielabdo <alloyali48@gmail.com>
2025-02-03 03:02:23 +0200

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,34 @@
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import react from './react.png';
+import java from './java.png';
+import python from './python.png';
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import tailwind from "./tech/tailwind.png";
+import jinan from "./company/jinan.png";
+import udemy from "./company/udemy.png";
+
+import robofriends from "./robofriends.png";
+
+export {
+  web,
+  github,
+  menu,
+  close,
+  css,
+  git,
+  html,
+  javascript,
+  tailwind,
+  jinan,
+  robofriends,
+  udemy,
+  react,
+  python,
+  java
+};
